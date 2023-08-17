433629be44e78c56f4234b013359be616aa864d4
EC2 Default User <ec2-user@ip-10-0-6-95.ap-southeast-1.compute.internal>
2023-08-17 15:57:05 +0000

    update

--- devops/setup-app.sh
+++ devops/setup-app.sh
@@ -8,10 +8,13 @@ sudo mv /tmp/env.txt /var/www/html/bagisto/.env
 sudo rm -rf /etc/nginx/nginx.conf
 sudo mv /tmp/nginx.conf /etc/nginx/nginx.conf
 
+sudo chown ec2-user:ec2-user /var/www/html/bagisto/.env
+sudo chmod 775 /var/www/html/bagisto/.env
+
 sudo php /var/www/html/bagisto/artisan key:generate
-sudo php /var/www/html/bagisto/artisan migrate --force
+sudo php /var/www/html/bagisto/artisan migrate
 sudo php /var/www/html/bagisto/artisan db:seed
-echo "0" | sudo php /var/www/html/bagisto/artisan vendor:publish
+sudo php /var/www/html/bagisto/artisan vendor:publish --tag=0
 sudo php /var/www/html/bagisto/artisan storage:link
 sudo chmod -R 777 /var/www/html/bagisto/storage/
 
